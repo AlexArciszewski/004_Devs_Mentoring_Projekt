a7bb57d88e4426e49c89ef2f5378221468f0e826
AlexArciszewski <arciszew@yahoo.com>
2023-07-03 20:02:22 +0200

    Trying to import class from cipher

--- menu.py
+++ menu.py
@@ -1,41 +1,15 @@
-from typing import Callable
-
-
 class Menu:
+    """Class connected to the main menu of the cipher program"""
+    START_MENU = """This is the main menu of cipher program. Please choose the right option to continue: """
 
 
     @staticmethod
-    def code_the_file():
-        print("code the file")
-
-    def uncode_the_file():
-        print("uncode the file")
+    def show_some_info():
+        """Crude menu text"""
+        print("This is the main menu of cipher program. Please choose the right option to continue: ")
 
 
-    def remove_from_list():
-        print("")
 
+    print(START_MENU) #opcja nr 1
 
-    options: dict[int, Callable] = {
-        1: code_the_file,
-            #("""To create new file press: "n". To open existing file press "o". To get back to main menu press "b".""" )
-            # jak wybierze nowy to powstanie notepad do zapisania....i prosba o pdoanie tekstu do zapisu lub powrót do menu
-            #jak wybierze o to uruchamia się wyszukiwarka lub nacisnij ba aby wrocic do poprzedniego menu...
-
-
-        2: uncode_the_file,
-            #po wybraniu bedzie wybierz plik do odkodowania jak w opcji wyżej : To open existing file press "o".
-            # To get back to main menu press "b".""" )
-            # jak wybierze o to uruchamia się wyszukiwarka lub nacisnij ba aby wrocic do poprzedniego menu...
-        3: remove_from_list,
-    }
-
-    user_choice = int(input("Welcome to code generator. Please choose right option from menu"))
-
-    if user_choice in options:
-        options.get(user_choice)
-
-    else:
-       print("Invalid option")
-
-
+Menu.show_some_info()       #opcja nr2
